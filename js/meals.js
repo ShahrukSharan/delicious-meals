cc9913e7d30312c1aabec82ebeab6d020f9c2aa6
Sharan <shahruksharan1590@gmail.com>
2022-09-01 19:39:52 +0600

    added search functionality

---
+++ js/meals.js
@@ -0,0 +1,58 @@
+const getMeals = (search) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+
+const displayMeals = (meals) => {
+  const mealsContainer = document.getElementById("meals-conatiner");
+  mealsContainer.innerHTML = ``;
+  meals.forEach((meal) => {
+    // console.log(meal);
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("col");
+    mealDiv.innerHTML = `
+        <div onclick="getMealDetails(${meal.idMeal})" class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+    mealsContainer.appendChild(mealDiv);
+  });
+};
+
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  getMeals(searchText);
+  searchField.value = "";
+};
+
+const getMealDetails = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+    console.log(meal);
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML = ``;
+  const detailDiv = document.createElement("div");
+  detailDiv.classList.add("card");
+  detailDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0,300)}</p>
+    </div>
+    `;
+  detailContainer.appendChild(detailDiv);
+};
+
+// getMeals('');
